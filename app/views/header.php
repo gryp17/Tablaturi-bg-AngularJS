65e96ae20e956ab907e4aaad9a06ebac6b84bb82
plamen <plamen@plamen-PC>
2016-04-27 22:21:54 +0300

    Added inner header content

--- app/views/header.php
+++ app/views/header.php
@@ -4,7 +4,30 @@
 	</div>
 	
 	<div class="inner-header">
-		<img src="static/img/logo-tablaturi-bg.png"/>
+		<div class="social-icons">
+			<a href="http://www.facebook.com/tablaturiBG" target="_blank">
+				<img src="static/img/social/fb-icon.jpg" title="Последвайте ни във Facebook"/>
+			</a>
+			<a href="https://plus.google.com/116630108688799411998" target="_blank" rel="publisher">
+				<img src="static/img/social/google-icon.jpg" title="Последвайте ни в Google+"/>
+			</a>
+		</div>
+		
+		<div class="authentication">
+			<a href="#signup">Регистрация</a>
+			<input class="btn btn-red" type="button" value="Вход"/>
+		</div>
+		
+		<div class="stats">
+			<div>
+				<span>52132</span> GuitarPro таблатури
+			</div>
+			<div>
+				<span>192451</span> Текстови таблатури
+			</div>
+		</div>
+		
+		<img src="static/img/logo-tablaturi-bg.png" title="Tablaturi-BG"/>
 	</div>
 </header>
 
--- app/views/layout.php
+++ app/views/layout.php
@@ -4,21 +4,53 @@
         <title>Tablaturi-BG</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700,600' rel='stylesheet' type='text/css'>
         <link href="static/stylesheets/style.min.css" rel="stylesheet" type="text/css"/>
         <script type="text/javascript" src="static/scripts/app.js"></script>		
     </head>
     <body>
         <div id="main-wrapper">
+
 			<?php include "header.php" ?>
 			<?php include "search.php" ?>
-			<div class="left-ads">
-			</div><!--
-		 --><section id="content-wrapper" ng-view="">
-			</section><!--
-		 --><div class="right-ads">
-			</div>
-			<div class="bottom-ad">
 
+			<div class="left-ads">
+				<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <!-- left-ad -->
+                <ins class="adsbygoogle"
+                     style="display:inline-block;width:120px;height:600px"
+                     data-ad-client="ca-pub-0878746760349023"
+                     data-ad-slot="9165853768"></ins>
+                <script>
+					(adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
+			</div>
+
+			<section id="content-wrapper" ng-view="">
+			</section>
+
+			<div class="right-ads">
+				<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <!-- right-ad -->
+                <ins class="adsbygoogle"
+                     style="display:inline-block;width:120px;height:600px"
+                     data-ad-client="ca-pub-0878746760349023"
+                     data-ad-slot="4596053365"></ins>
+                <script>
+					(adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
+			</div>
+
+			<div class="bottom-ad">
+				<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+				<!-- bottom-ad -->
+				<ins class="adsbygoogle"
+					 style="display:inline-block;width:728px;height:90px"
+					 data-ad-client="ca-pub-0878746760349023"
+					 data-ad-slot="6010250962"></ins>
+				<script>
+					(adsbygoogle = window.adsbygoogle || []).push({});
+				</script>
 			</div>
 			<?php include "footer.php" ?>
         </div>
