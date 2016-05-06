48206184db53d1ad8f26b35e3c44780b937681b9
plamen <plamen.ivanov17@gmail.com>
2016-05-06 22:51:32 +0300

    Finished the articles page

--- app/controllers/articles.js
+++ app/controllers/articles.js
@@ -4,13 +4,24 @@ app.controller("articlesController", function ($scope, ArticleService, LoadingSe
 	$scope.offset = 0;
 	$scope.articles = [];
 
+	/**
+	 * Loads the articles from the database
+	 * @param {int} limit
+	 * @param {int} offset
+	 */
 	$scope.loadArticles = function (limit, offset) {
 		ArticleService.getArticles(limit, offset).success(function (result) {
+			console.log(result.data);
 			if (result.error) {
 				console.log(result.error);
 			} else {
 				LoadingService.doneLoading();
-				$scope.articles.push(result.data);
+				$scope.articles = $scope.articles.concat(result.data);
+				
+				//hide the load more button if there are no more articles
+				if(result.data.length < $scope.limit){
+					$scope.noMoreArticles = true;
+				}
 			}
 		});
 	};
--- app/views/partials/articles.php
+++ app/views/partials/articles.php
@@ -1,11 +1,11 @@
 <div class="articles">
 	<h2 class="title">Новини:</h2>
 	<div class="row">
-		<div class="col-xs-6" ng-repeat="article in articles">
+		<div class="col-xs-6" ng-repeat="article in articles track by article.ID">
 			<div article article-data="article"></div>
 		</div>
 	</div>
-	<div class="view-more">
+	<div ng-show="!noMoreArticles" class="view-more">
 		<button ng-click="offset = offset + 6; loadArticles(limit, offset)" class="btn btn-red">Покажи още новини</button>
 	</div>
 </div>
