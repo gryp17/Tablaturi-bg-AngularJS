da46082c728c2aea7918b6e0e2c312f0fcc43b38
Plamen Ivanov <plamen.ivanov17@gmail.com>
2016-10-17 17:38:36 +0300

    Added the backing tracks user agent to the config

--- backend/config/Config.php
+++ backend/config/Config.php
@@ -10,6 +10,7 @@ class Config {
 	#backing tracks authentication
 	const BT_NAME = 'tablatur';
 	const BT_PASS = '1234';
+	const BT_UA = 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:17.0) Gecko/20100101 Firefox/17.0';
 	
 	#directories
 	const ARTICLES_DIR = 'content/articles/';
